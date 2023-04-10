8cddef4185b93616a3e58f258a4da2dd278f9034
JohannChambillaF <johannchambilla.f@gmail.com>
2023-04-09 20:03:08 -0500

    se creo funcion para abrir oficio

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,10 @@
-function CargarOficio(){
+function CargarConformidad(){
 	$.get('proyecto/conformidad.php', function(e){
+		$('#resultado').html(e);
+	})
+}
+function CargarOficio(){
+	$.get('proyecto/oficio.php', function(e){
 		$('#resultado').html(e);
 	})
 }
