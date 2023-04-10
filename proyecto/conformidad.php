9686a28e3c430ef6b72a2a57e1e5f5543f06e360
JohannChambillaF <johannchambilla.f@gmail.com>
2023-04-09 20:05:37 -0500

    muchos cambios no guarde antes

--- proyecto/conformidad.php
+++ proyecto/conformidad.php
@@ -4,9 +4,9 @@
 
 <body>
 <div style="margin: 15px; display: flex; flex-wrap: nowrap;">
-	<div class="card col-6"  style="margin-bottom: 15px;">
-		<div class="card-header" style="color: #FFFF;">
-			<H5>Registro Conformidades</H5>
+	<div class="card col-6"  style="margin-bottom: 15px; border-color:rgba(76, 150, 217);">
+		<div class="card-header" style="background-color: rgba(76, 150, 217);">
+			<H5 style="color: #FFFF;">Registro Conformidades</H5>
 		</div>
 		<div class="card-body text-center">
 			<form method="POST" id="frm_registrar">
@@ -17,16 +17,16 @@
 							<input type="date" name="fecreg" id="fecreg" class="form-control">
 						</div>
 						<div class="input-field">
-							<input type="text" name="numreg" id="numreg" placeholder="Nro Registro" class="form-control">
+							<input type="text" name="numreg" id="numreg" placeholder="Nro Registro" class="form-control" autocomplete="off">
 						</div>
 						<div class="input-field">
-							<input type="text" name="alumno" id="alumno" placeholder="Alumno" class="form-control">
+							<input type="text" name="alumno" id="alumno" placeholder="Alumno" class="form-control" autocomplete="off">
 						</div>
 						<div class="input-field" style="display: flex; flex-wrap: nowrap;">
 							<input type="text" name="codigo" id="codigo" placeholder="Código" class="form-control" autocomplete="off">	
 						</div>
 						<div class="input-field">
-							<select name="escuela" id="escuela" class="form-select">
+							<select name="escuela" id="escuela" class="form-select" required>
 								<?php 
 									$sql=$conexion->query("SELECT * FROM escuela");
 									while ($esc=mysqli_fetch_array($sql))
@@ -34,11 +34,10 @@
 										$id=$esc['idescuela'];
 										$nomesc=$esc['nombresc'];
 									?>
-									
+									<option hidden selected>Seleccione una Escuela</option>
 									<option value="<?=$id?>"><?=$nomesc?></option>
 									<?php
 									}
-
 								?>
 							</select>
 						</div>
@@ -46,7 +45,7 @@
 					<div class="col-sm-6">
 						<div class="input-field">
 							<select name="modalidad" id="modalidad" class="form-select">
-								
+								<option hidden selected>Seleccione una Modalidad</option>
 								<option value="ORDINARIO">ORDINARIO</option>
 								<option value="EXTRAORDINARIO">EXTRAORDINARIO</option>
 							</select>
@@ -60,7 +59,7 @@
 										$id=$sed['idsede'];
 										$nomsed=$sed['descripsede'];
 									?>
-									
+									<option hidden selected>Seleccione una Sede</option>
 									<option value="<?=$id?>"><?=$nomsed?></option>
 									<?php
 									}
@@ -68,13 +67,13 @@
 							</select>
 						</div>
 						<div class="input-field">
-							<input type="text" name="celular" value="" id="celular" placeholder="Celular" class="form-control">	
+							<input type="text" name="celular" value="" id="celular" placeholder="Celular" class="form-control" autocomplete="off">	
 						</div>
 						<div class="input-field">
-							<input type="text" name="correo" value="" id="correo" placeholder="Correo" class="form-control">	
+							<input type="text" name="correo" value="" id="correo" placeholder="Correo" class="form-control" autocomplete="off">	
 						</div>
 						<div class="input-field">
-							<input type="text" name="dni" value="" id="dni" placeholder="DNI" class="form-control">	
+							<input type="text" name="dni" value="" id="dni" placeholder="DNI" class="form-control" autocomplete="off">	
 						</div>
 					</div>
 				</div>
@@ -82,10 +81,11 @@
 					<button type="submit" class="btn btn-secondary btn-block" name="btn_guardar" id="btn_guardar" style="width: 100%">Guardar</button>
 				</div>
 			</form>
+			
 		</div>		
 	</div>
-	<div class="card col-6 ms-2"  style="margin-bottom: 15px;">
-		<div class="card-header" style="color: #FFFF;"><H5>Validación Expediente</H5>
+	<div class="card col-6 ms-2"  style="margin-bottom: 15px; border-color:rgba(76, 150, 217);">
+		<div class="card-header" style="background-color: rgba(76, 150, 217);"><H5 style="color: #FFFF;">Validación Expediente</H5>
 		</div>
 		<div class="card-body text-center">
 			<div id="buscacodigo"></div>			
@@ -93,14 +93,18 @@
 	</div>							
 </div>
 <div class="w3-container" style="margin: 15px;">
-	<div class="card">
-		<div class="card-header" style="color: #FFFF;"><H5>Lista Conformidades</H5></div>
-		<div class="card-body text-center">
-			<form method="POST" id="frm_bustabla">
+	<div class="card" style="border-color:rgba(76, 150, 217);">
+		<div class="card-header" style="background-color: rgba(76, 150, 217);"><H5 style="color: #FFFF;">Lista Conformidades</H5></div>
+		<div class="card-body">
+			<!--<form method="POST" id="frm_bustabla"> ESTE ES EL BUSCADORRRRRRRRRRRR
 				<div class="input-field">
 					<input type="text" name="busctabla" id="busctabla" placeholder="Buscar..." class="form-control" autocomplete="off" style="width: 20%;">
 				</div>
 			</form>
+			<div class="registros" id="agrega-registros"></div>
+			<center>
+				<ul class="pagination" id="pagination"></ul>
+			</center>-->
 			<table class="table table-striped table-bordered" align="vertical" id="tabla">
 				<thead class="table-primary" style='font-size: 12px; color: #626161;'>
 					<tr>
@@ -116,9 +120,8 @@
 						<th class="text-center" style="width: 15%;">ACCION</th>
 					</tr>
 				</thead>
-
-				<tbody style='font-size: 12px;' id="tablaconfor"><!--este id="tablaconfor" es para el buscador-->
-				<?php  
+				<tbody class="text-center" style='font-size: 12px;'><!--este id="tablaconfor" es para el buscador-->
+					<?php  
 
 					$sql = "SELECT r.idconfoficio,r.fechrecepcion,r.nregistro,r.alumno,r.codigo,e.nombresc,r.modalidad,s.descripsede,r.celular,r.correo,r.dni,r.estado 
 						FROM registro r 
@@ -152,9 +155,10 @@
 								
 							</tr>							
 					<?php }
-				?>
+					?>
 				</tbody>
 			</table>
+			<div class="alert alert-danger" role="alert">Solo se muestra los ultimos 7 registros... Para confirmar que se guardo en la BD</div>
 		</div>
 	</div>			
 </div>
@@ -167,6 +171,7 @@
 
 			e.preventDefault();//esto sirve para que la pagina no se recargue
 			agregar_datos();//esta llamando a la funcion creada en funciones.js
+
 		});
 	});	
 </script>
@@ -200,27 +205,59 @@
 <!--SCRIPT llamado DATATABLE-->
 <!--
 <script>
-	$(document).ready(function () {
-   		$('#tabla').DataTable({
-    		"pageLength": 5,
-			"order": [[ 0, "desc" ]], //or asc 
-   		});
-	});-->
-</script>
-<!--SCRIPT llamado ELIMINAR-->
-<script>
 	$(document).ready(function(){
-		$("#deleteconfor").on('click', function(e){//hace referencia a la accion de prescionar el boton
-			e.preventDefault();//esto sirve para que la pagina no se recargue
-			if(confirm('Estas seguro?'))
-			{
-				alert('yes');
-			}
-			
+		$('#tabla').DataTable({ 
+				"destroy":true,
+				"lengthMenu" : [5 ,10, 15, 20],
+				"info":false,
+				"ordering":false,
+				"order": [[ 0, "desc" ]], //or asc 
+				
+				dom: 'Bfrtip',//desaparece la casilla de filtro 5-10-15...
+				buttons: 
+				[
+					//'copy','excel', 'pdf'
+					{	
+						extend: 'excelHtml5',
+						text: '<i class="fa-regular fa-file-excel"></i>',
+						titleAttr: 'Exportar a Excel',
+						className: 'btn btn-success'
+					},
+					{	
+						extend: 'pdfHtml5',
+						text: '<i class="fa-solid fa-file-pdf"></i>',
+						titleAttr: 'Exportar a PDF',
+						className: 'btn btn-danger'
+					},
+					{	
+						extend: 'print',
+						text: '<i class="fa-solid fa-print"></i>',
+						titleAttr: 'Imprimir',
+						className: 'btn btn-info'
+					},
+				],
+				/*"ajax":{
+				    	"method":"POST",
+				    	"url": "controlador/tablaconfor.php"
+		    		},
+		    		"columns" : 
+		    		[
+		    			{"data":"idconfoficio"},
+		    			{"data":"fechrecepcion"},
+						{"data":"nregistro"},
+						{"data":"alumno"},
+						{"data":"codigo"},
+						{"data":"nombresc"},
+						{"data":"modalidad"},
+						{"data":"descripsede"},
+						{"data":"estado"},
+						{"defaultContent":"<a class='btn btn-warning' style='width: 30px; height: 35px;'><i class='material-symbols-outlined'>edit</i></a><a class='btn btn-danger' style='width: 30px; height: 35px;' name='deleteconfor'><i class='material-symbols-outlined'>delete</i></a>"}
+		    		]*/
 		});
-	});	
-</script>
+	});
+</script>-->
 <!--SCRIPT BUSQUEDA DE TABLA CONFORMIDAD-->
+<!--
 <script>
 	$(document).ready(function(){
 		$("#busctabla").on("keyup", function() {
@@ -230,6 +267,11 @@
 			});
 		});
 	});
-</script>
+	</script>-->
+<!--SCRIPT DE TABLA CONFORMIDAD 02-->
+<!--
+<script type="text/javascript">
+		$(document).ready(pagination(1));
+	</script>-->
 </body>
 </html>
