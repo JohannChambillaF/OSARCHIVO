a590a81c693439ff4a683821ab18d4cf6a4062a4
JohannChambillaF <johannchambilla.f@gmail.com>
2023-04-10 20:31:22 -0500

    se agrego al sidebar nuevos submneus

--- proyecto/componentes/sidebars.php
+++ proyecto/componentes/sidebars.php
@@ -1,22 +1,55 @@
 <div class="d-flex flex-column flex-shrink-0 p-2 text-white bg-dark" style="width: 280px;">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none" style="margin-top: 35px;" >
+    <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none" style="margin-top: 35px;" >
       <img src="librerias/img/libro.png" width="40" height="32"></svg>
       <span class="fs-4 ms-2">OSA ARCHIVO</span>
     </a>
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
       <li class="nav-item">
-        <a href="#" onclick="CargarConformidad()" class="nav-link text-white" aria-current="page" style="margin-top: 20px;" >
-          <i class="fa fa-house"></i>
-          <span class="ms-2 d-none d-sm-inline">Conformidad Doc</span>
+        <a href="#" onclick="CargarBusqueda()" class="nav-link text-white" aria-current="page" style="margin-top: 20px;" >
+          <i class="fa-solid fa-magnifying-glass"></i>
+          <span class="ms-2 d-none d-sm-inline">Busqueda</span>
         </a>
       </li>
+      <!---------------------------------------------->  
       <li class="nav-item">
-        <a href="#" onclick="CargarOficio()" class="nav-link text-white" aria-current="page" style="margin-top: 20px;" >
-          <i class="fa fa-gauge"></i>
-          <span class="ms-2 d-none d-sm-inline">Expedientes-Oficio</span>
+        <a href="#subconfor" data-bs-toggle="collapse" class="nav-link text-white" aria-current="page" style="margin-top: 10px;" >
+          <i class="fa-solid fa-file-circle-check"></i>
+          <span class="ms-2 d-none d-sm-inline">Conformidad Doc</span><i class="ms-2 fa-solid fa-angle-down"></i>
         </a>
+        <ul class="collapse nav flex-column ms-1" id="subconfor" data-bs-parent="#menu">
+            <li class="w-100">
+                <a href="#" onclick="CargarConformidad()" class="nav-link text-white px-5" aria-current="page" style="margin-top: 5px;" >
+                <i class="fa-regular fa-pen-to-square"></i>
+                <span class="d-none d-sm-inline">Registrar Confor</span></a>
+            </li>
+            <li class="w-100">
+            <a href="#" class="nav-link text-white px-5" aria-current="page" style="margin-top: 5px;" >
+                <i class="fa-solid fa-list-check"></i>
+                <span class="d-none d-sm-inline">Revisar Confor</span></a>
+            </li>
+        </ul>
       </li>
+      <!---------------------------------------------->  
+      <li class="nav-item">
+        <a href="#suboficio" data-bs-toggle="collapse" class="nav-link text-white" aria-current="page" style="margin-top: 10px;" >
+          <i class="fa-regular fa-folder-closed"></i>
+          <span class="ms-2 d-none d-sm-inline">Expedientes-Oficio</span><i class="ms-2 fa-solid fa-angle-down"></i>
+        </a>
+        <ul class="collapse nav flex-column ms-1" id="suboficio" data-bs-parent="#menu">
+            <li class="w-100">
+                <a href="#" onclick="CargarOficio()" class="nav-link text-white px-5" aria-current="page" style="margin-top: 5px;" >
+                <i class="fa-regular fa-pen-to-square"></i>
+                <span class="d-none d-sm-inline">Registrar Oficio</span></a>
+            </li>
+            <li class="w-100">
+            <a href="#" class="nav-link text-white px-5" aria-current="page" style="margin-top: 5px;" >
+                <i class="fa-solid fa-list-check"></i>
+                <span class="d-none d-sm-inline">Revisar Expediente</span></a>
+            </li>
+        </ul>
+      </li>
+      <!---------------------------------------------->  
       <li>
         <a href="#" class="nav-link text-white">
           <i class="fa fa-table"></i>
