2cd5debd64ebd418a73a3079f62b792c6dd681f5
JohannChambillaF <johannchambilla.f@gmail.com>
2023-04-19 05:38:05 -0500

    se modifico funcion agregar actualizar

--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,4 @@
-function agregar_datos(){ 
+function agregar_actuali_confor(){ 
 	var btn=$(this);
 	var id=$("#idconfoficio").val();
 
@@ -12,38 +12,20 @@ function agregar_datos(){
 		success:function(res){
 			
 			var id=$("#idconfoficio").val();
+			
 			if(id=="0"){
 				$("#tabla").find("tbody").append(res);
-			}else{
-				$("#tabla").find("."+id).html(res);
-			}
-			$("#btn_guardar").text("Add User");
-		}
-	});
-}
-/*function agregar_datos(){
-
-	var datos = $("#frm_registrar").serialize();
-
-	$.ajax({
-		
-		method: "POST",
-		url: "controlador/insertconfor.php",
-		data: datos,
-
-		success: function(e){
-
-			if (e==1){
-				
 				alert('registro exitoso');
 				$('#frm_registrar').trigger('reset');
 				$('#tabla').load('proyecto/conformidad.php #tabla');
-
 			}else{
-				alert('error de registro');
+				$("#tabla").find("."+id).html(res);
+				alert('registro actualizado');
+				$('#frm_registrar').trigger('reset');
+				$('#tabla').load('proyecto/conformidad.php #tabla');
+				$("#btn_guardar_actual").text("Guardar");
+				$("#btn_guardar_actual").css("btn-secondary");
 			}
 		}
 	});
-
-	return false;
-}*/
+}
