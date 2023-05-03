bcc069bf522caee208ece7290338d603a2c7b2ae
JohannChambillaF <johannchambilla.f@gmail.com>
2023-05-02 22:14:21 -0500

    guarda y actualiza en ofic

--- controlador/insertofic.php
+++ controlador/insertofic.php
@@ -3,27 +3,23 @@
 include "../conexion/conexion.php";
 
 $idconfoficio = $_POST['idconfoficio'];
-$confor = $_POST['confor'];
-$fecreg = $_POST['fecreg'];
-$numreg = $_POST['numreg'];
-$alumno = $_POST['alumno'];
-$codigo = $_POST['codigo'];
-$escuela = $_POST['escuela'];
-$modalidad = $_POST['modalidad'];
-$sede = $_POST['sede'];
-$celular = $_POST['celular'];
-$correo = $_POST['correo'];
-$dni = $_POST['dni'];
+$ofic = mysqli_real_escape_string($conexion,$_POST['ofic']);
+$fecreg = mysqli_real_escape_string($conexion,$_POST['fecreg']);
+$numreg = mysqli_real_escape_string($conexion,$_POST['numreg']);
+$noficio = mysqli_real_escape_string($conexion,$_POST['noficio']);
+$alumno = mysqli_real_escape_string($conexion,$_POST['alumno']);
+$codigo = mysqli_real_escape_string($conexion,$_POST['codigo']);
+$escuela = mysqli_real_escape_string($conexion,$_POST['escuela']);
+$modalidad = mysqli_real_escape_string($conexion,$_POST['modalidad']);
+$sede = mysqli_real_escape_string($conexion,$_POST['sede']);
+$estado = mysqli_real_escape_string($conexion,$_POST['estado']);
 
-if ($idconfoficio == "0"){
-    $sql = "CALL InsertarConf ('$confor','$fecreg','$numreg','$alumno','$codigo','$escuela','$modalidad','$sede','$celular','$correo','$dni')";
-
-
+if($idconfoficio == "0")
+{
+    $sql = "CALL InsertarOfic ('$ofic','$fecreg','$numreg','$noficio','$alumno','$codigo','$escuela','$modalidad','$sede','$estado')";
 }else{
-
+    $sql = "CALL ActualizarOfic ('$idconfoficio','$fecreg','$numreg','$noficio','$alumno','$codigo','$escuela','$modalidad','$sede')";
 }
-
-
 echo mysqli_query($conexion,$sql);
 
 ?> 
