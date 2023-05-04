1a5bb8e4940ccc8363fe1944f20d2621aec566e3
JohannChambillaF <johannchambilla.f@gmail.com>
2023-05-04 16:08:45 -0500

    se hizo la validacion

--- controlador/actualicompleto.php
+++ controlador/actualicompleto.php
@@ -1,21 +1,24 @@
 <?php 
 include "../conexion/conexion.php";
 
-$idconfoficio = $_POST['idconfoficio'];
-$estado = mysqli_real_escape_string($conexion,$_POST['estado']);
-$alumno = mysqli_real_escape_string($conexion,$_POST['alumno']);
-$escuela = mysqli_real_escape_string($conexion,$_POST['escuela']);
-$modalidad = mysqli_real_escape_string($conexion,$_POST['modalidad']);
-$sede = mysqli_real_escape_string($conexion,$_POST['sede']);
-$fecaten = mysqli_real_escape_string($conexion,$_POST['fecaten']);
-$numate = mysqli_real_escape_string($conexion,$_POST['numate']);
-$ubicexp = mysqli_real_escape_string($conexion,$_POST['ubicexp']);
-$observ = mysqli_real_escape_string($conexion,$_POST['observ']);
+if(!empty ($_POST['estado'])) { 
 
+    $idconfoficio = $_POST['idconfoficio'];
+    $estado = mysqli_real_escape_string($conexion,$_POST['estado']);
+    $alumno = mysqli_real_escape_string($conexion,$_POST['alumno']);
+    $escuela = mysqli_real_escape_string($conexion,$_POST['escuela']);
+    $modalidad = mysqli_real_escape_string($conexion,$_POST['modalidad']);
+    $sede = mysqli_real_escape_string($conexion,$_POST['sede']);
+    $fecaten = mysqli_real_escape_string($conexion,$_POST['fecaten']);
+    $numate = mysqli_real_escape_string($conexion,$_POST['numate']);
+    $ubicexp = mysqli_real_escape_string($conexion,$_POST['ubicexp']);
+    $observ = mysqli_real_escape_string($conexion,$_POST['observ']);
 
-
-$sql = "CALL ActuaConfCompl	($idconfoficio,'$estado','$alumno','$escuela','$modalidad','$sede','$fecaten','$numate','$ubicexp','$observ')";
-
-echo mysqli_query($conexion,$sql);
+    $sql = "CALL ActuaConfCompl	($idconfoficio,'$estado','$alumno','$escuela','$modalidad','$sede','$fecaten','$numate','$ubicexp','$observ')";
+    echo mysqli_query($conexion,$sql);
+}
+else{
+    
+}    
 
 ?> 
