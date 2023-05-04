f7b5ac0576131feb5470f246b0929fb0244ce129
JohannChambillaF <johannchambilla.f@gmail.com>
2023-05-04 16:11:27 -0500

    se aumento la tabla en la parte de abajo

--- proyecto/revisarconfor.php
+++ proyecto/revisarconfor.php
@@ -22,7 +22,7 @@
 							<div class="col-6"><button class="btn btn-outline-warning" style="width:70px; height:38px;" id="filtro">Filtrar</button></div>
 							<div class="col-6 text-center" >
 								<?php
-									$sql = "SELECT * FROM registro	WHERE tipo = 'CONFORMIDAD' && estado = 'INCOMPLETO'";
+									$sql = "SELECT * FROM registro	WHERE tipo = 'CONFORMIDAD' && estado = 'REGISTRADO'";
 									$query = mysqli_query($conexion, $sql);
 									$total   = mysqli_num_rows($query);
 									echo'<div class="alert alert-success" style="height: 38px;padding: 5px;">Total: ('. $total .')</div>';
@@ -43,7 +43,7 @@
 						<th class="text-center">ESCUELA</th>
 						<th class="text-center">MODALIDAD</th>
 						<th class="text-center">SEDE</th>
-						<th class="text-center" style="width: 15%;">FECHA RECEP</th>
+						<th class="text-center" style="width: 15%;">FECHA REGIS</th>
 						<th class="text-center" style="display: none;">FECHA ENVIO</th>
 						<th class="text-center" style="display: none;">NRO ATENCION</th>
 						<th class="text-center" style="display: none;">UBICACION</th>
@@ -59,7 +59,7 @@
 						INNER JOIN escuela e ON r.idescuela = e.idescuela
 						INNER JOIN sede s ON r.idsede = s.idsede
 						INNER JOIN modalidad m ON r.idmodalidad = m.idmodalidad
-						WHERE r.tipo = 'CONFORMIDAD' && r.estado = 'INCOMPLETO'
+						WHERE r.tipo = 'CONFORMIDAD' && r.estado = 'REGISTRADO'
 						ORDER BY idconfoficio DESC LIMIT 7";
 
 					$ejecutar = mysqli_query($conexion, $sql);
@@ -73,12 +73,12 @@
 							<tr>
 								<td style="display: none;"><?=$fila->idconfoficio?></td>
 								<?php 
-									if($fila->estado =='INCOMPLETO')
-										echo '<td><img src="librerias/img/off.png" alt="" width="40" height="25"></td>';
+									if($fila->estado =='REGISTRADO')
+									echo '<td><img src="librerias/img/mediun.png" alt="" width="40" height="25"></td>';
 									if($fila->estado =='COMPLETO')
 									echo '<td><img src="librerias/img/on.png" alt="" width="40" height="25"></td>';
-									if($fila->estado =='PROBLEMA')
-									echo '<td><img src="librerias/img/mediun.png" alt="" width="40" height="25"></td>';
+									if($fila->estado =='OBSERVADO')
+									echo '<td><img src="librerias/img/off.png" alt="" width="40" height="25"></td>';
 								?>
 								<td><?=$fila->alumno?></td>
 								<td><?=$fila->nombresc?></td>
@@ -108,7 +108,7 @@
 				<div class="row">
 					<div class="col-sm-6">
 						<input type="hidden" class="form-control" name='idconfoficio' id='idconfoficio' value="0">
-						<input type="hidden" name="estado" id="estado" value="INCOMPLETO">					
+						<input type="hidden" name="estado" id="estado">					
 
 						<div class="input-field">
 							<input type="text" name="alumno" id="alumno" placeholder="Alumno" class="form-control" >
@@ -176,8 +176,8 @@
                     </div>
 				</div>
 				<div class="form-group" style="text-align: center;">
-					<input class="option-input radio" type="radio" name="estado" value="INCOMPLETO">
-					<label class="form-check-label" for="inlineRadio1">INCOMPLETO</label>
+					<input class="option-input radio" type="radio" name="estado" value="OBSERVADO">
+					<label class="form-check-label" for="inlineRadio1">OBSERVADO</label>
 					<input class="option-input radio" type="radio" name="estado" value="COMPLETO">
 					<label class="form-check-label" for="inlineRadio2">COMPLETO</label>
 					<input class="option-input radio" type="radio" name="estado" value="PROBLEMA">
@@ -192,10 +192,82 @@
 </div>
 <div class="w3-container" style="margin: 15px;">
 	<div class="card" style="border-color:rgba(76, 150, 217);">
-		<div class="card-header" style="background-color: rgba(76, 150, 217);"><H5 style="color: #FFFF;">Lista Conformidades</H5></div>
+		<div class="card-header" style="background-color: rgba(76, 150, 217);"><H5 style="color: #FFFF;">XXXX CAMPO LIBRE</H5></div>
 		<div class="card-body">
-			
-			<div class="alert alert-danger" role="alert">Solo se muestra los ultimos 7 registros... Para confirmar que se guardo en la BD</div>
+			<table class="table table-striped table-bordered" align="vertical" id="tablaconfcomp">
+				<thead class="table-primary" style='font-size: 12px; color: #626161;'>
+					<tr>
+						<th class="text-center" style="display: none;">ID</th>
+						<th class="text-center">ESTADO</th>
+						<th class="text-center">F REGISTRO</th>
+						<th class="text-center">N° REGISTRO</th>
+						<th class="text-center" style="width: 13%;">ALUMNO</th>
+						<th class="text-center">CÓDIGO</th>
+						<th class="text-center">ESCUELA</th>
+						<th class="text-center" style="width: 13%;">MODALIDAD</th>
+						<th class="text-center">SEDE</th>
+						<th class="text-center">F ENVIO</th>
+						<th class="text-center">NRO ATENCION</th>
+						<th class="text-center">UBICACION</th>
+						<th class="text-center">OBSERVACION</th>
+						<th class="text-center" style="width: 7%;">ACCION</th>
+					</tr>
+				</thead>
+				<tbody class="text-center" style='font-size: 12px;'><!--este id="tablaconfor" es para el buscador-->
+					<?php  
+
+					$sql = "SELECT r.idconfoficio ,r.fechrecepcion,r.nregistro,r.alumno,r.codigo,e.nombresc,m.descmod,s.descripsede,r.estado,r.fechenvio, r.atencion, r.ubicacion, r.observacion 
+						FROM registro r 
+						INNER JOIN escuela e ON r.idescuela = e.idescuela
+						INNER JOIN sede s ON r.idsede = s.idsede
+						INNER JOIN modalidad m ON r.idmodalidad = m.idmodalidad
+						WHERE r.tipo = 'CONFORMIDAD' && r.estado != 'REGISTRADO' 
+						ORDER BY fechenvio DESC LIMIT 5";
+
+					$ejecutar = mysqli_query($conexion, $sql);
+
+					while ($fila =mysqli_fetch_object($ejecutar))
+						//mysqli_fetch_array jala los datos de la BD como arrays (esto sirve cuando una tabla tiene nombres con columnas iguales esta es una forma de diferenciarlas ya que si se duplicasa nombre al llamar datos de BD habria errores)
+
+						//mysqli_fetch_object jala los datos pero con los mismos nombres como estan las columnas en la BD 
+						{ 
+						?>
+							<tr>
+								<td style="display: none;"><?=$fila->idconfoficio?></td>
+								<?php 
+									if($fila->estado =='PROBLEMA')
+										echo '<td><img src="librerias/img/problem.png" alt="" width="40" height="25"></td>';
+									if($fila->estado =='COMPLETO')
+									echo '<td><img src="librerias/img/on.png" alt="" width="40" height="25"></td>';
+									if($fila->estado =='OBSERVADO')
+									echo '<td><img src="librerias/img/off.png" alt="" width="40" height="25"></td>';
+								?>
+								<td><?=$fila->fechrecepcion?></td>
+								<td><?=$fila->nregistro?></td>
+								<td><?=$fila->alumno?></td>
+								<td><?=$fila->codigo?></td>
+								<td><?=$fila->nombresc?></td>
+								<td><?=$fila->descmod?></td>
+								<td><?=$fila->descripsede?></td>
+								<td><?=$fila->fechenvio?></td>
+								<td><?=$fila->atencion?></td>
+								<td><?=$fila->ubicacion?></td>
+								<td><?=$fila->observacion?></td>
+								<td>
+								<a href="#" class="btn btn-warning edit" idconfoficio="<?=$fila->idconfoficio?>" style="width: 30px; height: 35px;"><i class="material-symbols-outlined">edit</i></a>
+								</td>
+								
+							</tr>							
+					<?php }
+					?>
+				</tbody>
+			</table>
+			<div class="alert alert-danger" role="alert">Solo se muestra los ultimos 5 registro...  
+				<img src="librerias/img/on.png" width="40" height="25"> COMPLETO
+				<img src="librerias/img/off.png" width="40" height="25"> OBSERVADO
+				<img src="librerias/img/problem.png" width="40" height="25"> PROBLEMA
+				<img src="librerias/img/mediun.png" width="40" height="25"> REGISTRADO
+			</div>
 		</div>
 	</div>			
 </div>
@@ -244,7 +316,7 @@
 			$("#idconfoficio").val(idconfoficio);
 
 			var fila=$(this);
-
+			
 			var alumno=fila.closest("tr").find("td:eq(2)").text();
 			$("#alumno").val(alumno);
 
@@ -273,6 +345,48 @@
 			$("#ubicexp").val(ubic);
 			
 			var observac=fila.closest("tr").find("td:eq(10)").text();
+			$("#observ").val(observac);
+		});
+	});
+</script>
+<script>
+	$(document).ready(function(){
+		
+		$("body").on("click",".edit",function(e){
+			e.preventDefault();
+			var idconfoficio=$(this).attr("idconfoficio");
+			$("#idconfoficio").val(idconfoficio);
+
+			var fila=$(this);
+			
+			var alumno=fila.closest("tr").find("td:eq(4)").text();
+			$("#alumno").val(alumno);
+
+			var escuela=fila.closest("tr").find("td:eq(6)").text();
+			$("#escuela option").filter(function() {
+			    return $(this).text() == escuela;
+			  }).prop("selected", true);
+			
+			var modalidad=fila.closest("tr").find("td:eq(7)").text();
+			$("#modalidad option").filter(function() {
+			    return $(this).text() == modalidad;
+			  }).prop("selected", true);
+
+			var sede=fila.closest("tr").find("td:eq(8)").text();
+			$("#sede option").filter(function() {
+			    return $(this).text() == sede;
+			  }).prop("selected", true);
+
+			var fechenvio=fila.closest("tr").find("td:eq(9)").text();
+			$("#fecaten").val(fechenvio);
+
+			var natencion=fila.closest("tr").find("td:eq(10)").text();
+			$("#numate").val(natencion);
+			
+			var ubic=fila.closest("tr").find("td:eq(11)").text();
+			$("#ubicexp").val(ubic);
+			
+			var observac=fila.closest("tr").find("td:eq(12)").text();
 			$("#observ").val(observac);
 		});
 	});
